fbf64132fe8027e2e34f3a28191008dd8a595946
trayvonn <513168326@qq.com>
2020-09-22 09:11:27 +0800

    +++

--- src/main/java/com/java/concurrent/jdk8Lock/StampedLockDemo.java
+++ src/main/java/com/java/concurrent/jdk8Lock/StampedLockDemo.java
@@ -1,8 +1,15 @@
 package com.java.concurrent.jdk8Lock;
 
+import java.util.concurrent.locks.StampedLock;
+
 /**
  * @author 吴邪
  * @date 2020/8/20 15:51
  */
-public class StampedLockDemo {
+public class StampedLockDemo extends ReentrantLockDemo {
+    private static StampedLock stampedLock = new StampedLock();
+
+    public static void main(String[] args) {
+    }
+
 }
