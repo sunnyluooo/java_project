baa817b3bebcd51ddf410afc4cbd5dd9067cc893
trayvonn <513168326@qq.com>
2020-09-28 17:08:35 +0800

    +++

--- src/main/java/com/java/concurrent/jdk8Lock/StampedLockDemo.java
+++ src/main/java/com/java/concurrent/jdk8Lock/StampedLockDemo.java
@@ -10,6 +10,15 @@ public class StampedLockDemo extends ReentrantLockDemo {
     private static StampedLock stampedLock = new StampedLock();
 
     public static void main(String[] args) {
+        long write = stampedLock.writeLock();
+        long read = stampedLock.tryOptimisticRead();
+        try {
+            read();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        stampedLock.writeLock();
+
     }
 
 }
